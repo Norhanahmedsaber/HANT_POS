43e6e43c2a44d026aa0125941d9097a46320bca4
anazz12312 <anashesham000@gmail.com>
2022-12-12 16:00:20 +0200

    afsdhjshdlfhsdf

--- src/Services/LogServices.java
+++ src/Services/LogServices.java
@@ -9,31 +9,7 @@ import java.util.UUID;
 public class LogServices implements ILogServices{
     static ArrayList<Log> logs = new ArrayList<Log>();
     
-    public LogServices(){
-        
-        Log log=new Log();
-        log.id = UUID.randomUUID();
-        log.userName="hussien";
-        log.userRole= "admin";
-        log.action="created";
-        log.actedOn="customer";
-        log.actedOnName="anas";
-        log.date= new Date(); 
-        logs.add(log);
-        
-        
-        log= new Log();
-        log.id= UUID.randomUUID();
-        log.userName="sayed";
-        log.userRole= "koko";
-        log.action="updated";
-        log.actedOn="item";
-        log.actedOnName="TV";
-        log.date= new Date(); 
-        logs.add(log);
-
-    }
-
+    
     @Override
     public void deleteLog(UUID logId) {
      logs.remove(getById(logId));
