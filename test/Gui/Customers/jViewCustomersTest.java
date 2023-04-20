535ff981e6ddee4894f9cd2118cb7ea24b484979
2152001 <tokaabdelgawad60@gmail.com>
2023-04-20 14:54:55 +0200

    done

---
+++ test/Gui/Customers/jViewCustomersTest.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Gui.Customers;
+
+import java.awt.Color;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author DELL
+ */
+public class jViewCustomersTest {
+    
+    public jViewCustomersTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of changecolor method, of class jViewCustomers.
+     */
+    @Test
+    public void testChangecolor() {
+        System.out.println("changecolor");
+        Color c1 = null;
+        Color c2 = null;
+        Color c3 = null;
+        Color c4 = null;
+        Color c5 = null;
+        jViewCustomers instance = null;
+        instance.changecolor(c1, c2, c3, c4, c5);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of renderData method, of class jViewCustomers.
+     */
+    @Test
+    public void testRenderData() {
+        System.out.println("renderData");
+        jViewCustomers instance = null;
+        instance.renderData();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of updateData method, of class jViewCustomers.
+     */
+    @Test
+    public void testUpdateData() {
+        System.out.println("updateData");
+        jViewCustomers instance = null;
+        instance.updateData();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
