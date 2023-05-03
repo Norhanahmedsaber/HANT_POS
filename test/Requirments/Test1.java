2e439c8c978e4d04d95eaf65c1154b2b22d4670a
anas12312 <anashesham000@gmail.com>
2023-05-03 13:37:28 +0300

    ..

--- test/Requirments/Test1.java
+++ test/Requirments/Test1.java
@@ -5,7 +5,9 @@
  */
 package Requirments;
 
+import Entities.Item;
 import Entities.Role;
+import Entities.User;
 import Gui.Users.*;
 import Gui.jHomePage;
 import Gui.jMainPage;
@@ -16,6 +18,9 @@ import Services.LogServices;
 import Services.RoleServices;
 import Services.UserServices;
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.UUID;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -68,15 +73,86 @@ public class Test1 {
         role3.canViewLogs = false;
         roleServices.create(role3);
     }
+    private void setUpUsers() {
+        Role admin = roleServices.getByName("admin");
+        User user1 = new User();
+        user1.name = "Hussien";
+        user1.userName = "admin";
+        user1.password = "00000000";
+        user1.role = admin;
+        userServices.create(user1);
+        
+        Role role1 = roleServices.getByName("employee1");
+        User user2 = new User();
+        user2.name = "Anas Hesham";
+        user2.userName = "user1";
+        user2.password = "00000000";
+        user2.role = role1;
+        userServices.create(user2);
+        
+        Role role2 = roleServices.getByName("employee2");
+        User user3 = new User();
+        user3.name = "Anas Hesham";
+        user3.userName = "user2";
+        user3.password = "00000000";
+        user3.role = role2;
+        userServices.create(user3);
+        
+        Role role3 = roleServices.getByName("employee3");
+        User user4 = new User();
+        user4.name = "Anas Hesham";
+        user4.userName = "user3";
+        user4.password = "00000000";
+        user4.role = role3;
+        userServices.create(user4);
+
+    }
+    private Item getItem() {
+        Item item1 = new Item();
+        Calendar cl = Calendar.getInstance();
+        
+        item1.name="Oven";
+        item1.category="Electrical devices";
+        item1.description="Toshiba";
+        cl.setTime(new Date());
+        cl. add(Calendar.MONTH, 1);
+        item1.createdAt= cl.getTime();
+        item1.price=1000;
+        
+        return item1;
+    }
     @Before
     public void setUp() {
         userServices.deleteAll();
         roleServices.deleteAll();
         setUpRoles();
+        setUpUsers();
     }
     @Test
-    public static void testCase1() {
+    public void testCase1() {
         //Login
+        User user = accountServices.login("user3", "0000000"); // wrong password
+        assertNull(user);
+        
+        // No Auth
+        user = accountServices.login("user3", "00000000"); // user with no auth
+        assertNotNull(user);
+        
+        assertFalse(user.role.canCreateItem);
+        
+        // Can create item only
+        user = accountServices.login("user1", "00000000"); // user can only create item 
+        assertNotNull(user);
+        
+        assertTrue(user.role.canCreateItem);
+        
+        Item item = getItem();
+        itemServices.create(item);
+        
+        //user can create customer and item
+        
+        //admin
+        
     }
   
 
